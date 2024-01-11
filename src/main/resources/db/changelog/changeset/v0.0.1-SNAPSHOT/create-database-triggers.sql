ad2b85187afa14493407d9040ceba6bc04fac667
Dmitri.Djourov <Dmitri.Djourov@gmail.com>
2024-01-11 23:06:52 +0200

    added changelogs

--- src/main/resources/db/changelog/changeset/v0.0.1-SNAPSHOT/create-database-triggers.sql
+++ src/main/resources/db/changelog/changeset/v0.0.1-SNAPSHOT/create-database-triggers.sql
@@ -1,39 +1,33 @@
 -- liquibase formatted sql
 
 -- changeset DmitriDjourov:createTriggers
-CREATE TRIGGER before_insert_manager
+CREATE TRIGGER IF NOT EXISTS before_insert_manager
     BEFORE INSERT ON bankapp.manager
     FOR EACH ROW
-    SET NEW.m_id = IFNULL(NEW.m_id, UUID_TO_BIN(UUID())),
-        NEW.m_create_at = IFNULL(NEW.m_create_at, LOCALTIME);
+    SET NEW.m_id = IFNULL(NEW.m_id, UUID_TO_BIN(UUID()));
 
-CREATE TRIGGER before_insert_product
+CREATE TRIGGER IF NOT EXISTS before_insert_product
     BEFORE INSERT ON bankapp.product
     FOR EACH ROW
-    SET NEW.p_id = IFNULL(NEW.p_id, UUID_TO_BIN(UUID())),
-        NEW.p_create_at = IFNULL(NEW.p_create_at, LOCALTIME);
+    SET NEW.p_id = IFNULL(NEW.p_id, UUID_TO_BIN(UUID()));
 
-CREATE TRIGGER before_insert_client
+CREATE TRIGGER IF NOT EXISTS before_insert_client
     BEFORE INSERT ON bankapp.client
     FOR EACH ROW
-    SET NEW.c_id = IFNULL(NEW.c_id, UUID_TO_BIN(UUID())),
-        NEW.c_create_at = IFNULL(NEW.c_create_at, LOCALTIME);
+    SET NEW.c_id = IFNULL(NEW.c_id, UUID_TO_BIN(UUID()));
 
-CREATE TRIGGER before_insert_account
+CREATE TRIGGER IF NOT EXISTS before_insert_account
     BEFORE INSERT ON bankapp.account
     FOR EACH ROW
-    SET NEW.a_id = IFNULL(NEW.a_id, UUID_TO_BIN(UUID())),
-        NEW.a_create_at = IFNULL(NEW.a_create_at, LOCALTIME);
+    SET NEW.a_id = IFNULL(NEW.a_id, UUID_TO_BIN(UUID()));
 
-CREATE TRIGGER before_insert_transaction
+CREATE TRIGGER IF NOT EXISTS before_insert_transaction
     BEFORE INSERT ON bankapp.transaction
     FOR EACH ROW
-    SET NEW.t_id = IFNULL(NEW.t_id, UUID_TO_BIN(UUID())),
-        NEW.t_create_at = IFNULL(NEW.t_create_at, LOCALTIME);
+    SET NEW.t_id = IFNULL(NEW.t_id, UUID_TO_BIN(UUID()));
 
-CREATE TRIGGER before_insert_agreement
+CREATE TRIGGER IF NOT EXISTS before_insert_agreement
     BEFORE INSERT ON bankapp.agreement
     FOR EACH ROW
-    SET NEW.ag_id = IFNULL(NEW.ag_id, UUID_TO_BIN(UUID())),
-        NEW.ag_create_at = IFNULL(NEW.ag_create_at, LOCALTIME);
+    SET NEW.ag_id = IFNULL(NEW.ag_id, UUID_TO_BIN(UUID()));
 
