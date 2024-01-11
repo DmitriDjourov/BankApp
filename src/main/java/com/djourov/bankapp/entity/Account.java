92a8323720441587747adbc8b1a9c7cc4f4b364e
Dmitri.Djourov <Dmitri.Djourov@gmail.com>
2024-01-11 02:59:45 +0200

    Account enams @ManyToOne

--- src/main/java/com/djourov/bankapp/entity/Account.java
+++ src/main/java/com/djourov/bankapp/entity/Account.java
@@ -53,7 +53,7 @@ public class Account {
     @Column(name = "a_update_at")
     private LocalDate updatedAt;
 
-    @OneToOne(cascade = {MERGE, PERSIST, REFRESH}, fetch = FetchType.LAZY)
+    @ManyToOne(cascade = {MERGE, PERSIST, REFRESH}, fetch = FetchType.LAZY)
     @JoinColumn(name = "a_client_id", referencedColumnName = "c_id")
     private Client client;
 
