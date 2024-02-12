7c8722cb8872e7047860b8bc3f5ab4541dc88f90
Dmitri.Djourov <Dmitri.Djourov@gmail.com>
2024-02-12 19:33:06 +0200

    added new Unchecker version

--- src/main/java/com/djourov/bankapp/validation/constraint/UuidAnnotationChecker.java
+++ src/main/java/com/djourov/bankapp/validation/constraint/UuidAnnotationChecker.java
@@ -4,14 +4,23 @@ import com.djourov.bankapp.validation.annotation.UuidChecker;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-public class UuidAnnotationChecker implements ConstraintValidator<UuidChecker, String> {
+import java.util.Optional;
+import java.util.UUID;
+
+public class UuidAnnotationChecker implements ConstraintValidator<UuidChecker, UUID> {
+    private static final String UUID_PATTERN = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$";
+
     @Override
     public void initialize(UuidChecker constraintAnnotation) {
         ConstraintValidator.super.initialize(constraintAnnotation);
     }
 
     @Override
-    public boolean isValid(String value, ConstraintValidatorContext context) {
-        return true;
+    public boolean isValid(UUID value, ConstraintValidatorContext context) {
+        return Optional.ofNullable(value)
+                       .map(Object::toString)
+                       .filter(s -> !s.isBlank())
+                       .map(s -> s.matches(UUID_PATTERN))
+                       .orElse(false);
     }
 }
