a5b4939c7aa7ea4d756c2a87f91c62648bf380ba
Dmitri.Djourov <Dmitri.Djourov@gmail.com>
2024-02-20 13:24:35 +0200

    added @Operation in AccountController

--- src/main/java/com/djourov/bankapp/controller/rest/AccountController.java
+++ src/main/java/com/djourov/bankapp/controller/rest/AccountController.java
@@ -3,6 +3,10 @@ package com.djourov.bankapp.controller.rest;
 import com.djourov.bankapp.dto.AccountDto;
 import com.djourov.bankapp.entity.Account;
 import com.djourov.bankapp.service.interf.AccountService;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.Hibernate;
 import org.springframework.web.bind.annotation.*;
@@ -17,21 +21,89 @@ public class AccountController {
     private final AccountService accountService;
 
     @GetMapping("/accounts")//localhost:8080/app/account/accounts
+    @Operation(summary = "The method returns information about accounts from the accounts table",
+            description = "The method returns the values of all fields of all records from the accounts table",
+            tags = "Accounts",
+            externalDocs = @ExternalDocumentation(
+                    description = "Documentation",
+                    url = "https://google.com/"
+            ),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Found"),
+                    @ApiResponse(responseCode = "500", description = "Not Found")
+            },
+            security = {
+                    @SecurityRequirement(name = "security requirements")
+            },
+            hidden = false
+    )
     public List<Account> getAllAccounts() {
         return accountService.getAllAccounts();
     }
 
     @GetMapping("/account_client_manager/{id}")// localhost:8080/app/account/account_client_manager/30633730-6166-6131-2d63-3635342d3437
+    @Operation(summary = "This method returns information about the account by its identifier.",
+            description = "TThis method returns a account record from the accounts table by the unique account" +
+                                  " identifier. The information includes fields from the accounts table, the first" +
+                                  " and last name of the account owner and the status of the manager servicing this" +
+                                  " account.",
+            tags = "Accounts",
+            externalDocs = @ExternalDocumentation(
+                    description = "Documentation",
+                    url = "https://google.com/"
+            ),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Found"),
+                    @ApiResponse(responseCode = "500", description = "Not Found")
+            },
+            security = {
+                    @SecurityRequirement(name = "security requirements")
+            },
+            hidden = false
+    )
     public AccountDto getAccountAndClientAndManger(@PathVariable("id") UUID id){
         return accountService.getACMId(id);
     }
 
     @GetMapping("/{id}")//localhost:8080/app/account/30633730-6166-6131-2d63-3635342d3437(не работает)
+    @Operation(summary = "The method returns information about account by its identifier",
+            description = "This method returns a complete account record from the accounts table by the account's unique identifier.",
+            tags = "Accounts",
+            externalDocs = @ExternalDocumentation(
+                    description = "Documentation",
+                    url = "https://google.com/"
+            ),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Found"),
+                    @ApiResponse(responseCode = "500", description = "Not Found")
+            },
+            security = {
+                    @SecurityRequirement(name = "security requirements")
+            },
+            hidden = false
+    )
     public Account getAccountByID(@PathVariable("id") UUID id) {
         Account account = accountService.getAccById(id);
         return (Account) Hibernate.unproxy(account);
     }
     @GetMapping("/account_number/{a_account_number}")//localhost:8080/app/account/account_number/123456789
+    @Operation(summary = "The method returns information about account by its account number",
+            description = "This method returns a complete account record from the accounts table by the account's " +
+                                  "unique account number.",
+            tags = "Accounts",
+            externalDocs = @ExternalDocumentation(
+                    description = "Documentation",
+                    url = "https://google.com/"
+            ),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Found"),
+                    @ApiResponse(responseCode = "500", description = "Not Found")
+            },
+            security = {
+                    @SecurityRequirement(name = "security requirements")
+            },
+            hidden = false
+    )
     public Account findAccountByAccountNumber(@PathVariable("a_account_number") String a_account_number) {
         return accountService.findAccountByAccountNumber(a_account_number);
     }
