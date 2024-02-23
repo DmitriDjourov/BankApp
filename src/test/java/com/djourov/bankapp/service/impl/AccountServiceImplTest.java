3b11ecc04d8e5a277e6e5f1e341ae39a30dd5c3c
Dmitri.Djourov <Dmitri.Djourov@gmail.com>
2024-02-23 23:39:31 +0200

    added AccountServiceImplTest done

--- src/test/java/com/djourov/bankapp/service/impl/AccountServiceImplTest.java
+++ src/test/java/com/djourov/bankapp/service/impl/AccountServiceImplTest.java
@@ -5,59 +5,77 @@ import com.djourov.bankapp.entity.Account;
 import com.djourov.bankapp.exception.AccountByIdNotFoundException;
 import com.djourov.bankapp.mapper.AccountMapper;
 import com.djourov.bankapp.repository.AccountRepository;
-import com.djourov.bankapp.service.interf.AccountService;
 import com.djourov.bankapp.util.DtoCreator;
 import com.djourov.bankapp.util.EntityCreator;
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.mockito.Mockito.clearInvocations;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class AccountServiceImplTest {
     @Mock
-    AccountRepository accountRepository;
-    AccountService accountService;
+    private AccountRepository accountRepository;
     @Mock
-    AccountMapper accountMapper;
+    private AccountMapper accountMapper;
+    @InjectMocks
+    private AccountServiceImpl accountService;
 
-    @BeforeEach
-    void init() {
-        accountService = new AccountServiceImpl(accountRepository, accountMapper);
+    @AfterEach
+    public void clearMocks() {
+        clearInvocations(accountMapper, accountMapper);
     }
 
     @Test
-    void getACMId_ShouldReturnAAccount() {
-        UUID id = EntityCreator.getAccount().getId();
-        Account account = new Account(id,
-                EntityCreator.getAccount().getAccountNumber(),
-                EntityCreator.getAccount().getType(),
-                EntityCreator.getAccount().getStatus(),
-                EntityCreator.getAccount().getBalance(),
-                EntityCreator.getAccount().getCurrencyCode(),
-                EntityCreator.getAccount().getCreatedAt(),
-                EntityCreator.getAccount().getUpdatedAt(),
-                null, null, null, null);
-        AccountDto accountDto = new AccountDto(DtoCreator.getAccountDto().getId(),
-                DtoCreator.getAccountDto().getAccountNumber(),
-                DtoCreator.getAccountDto().getFirstName(),
-                DtoCreator.getAccountDto().getLastName(),
-                DtoCreator.getAccountDto().getStatus());
-        when(accountRepository.findById(id)).thenReturn(Optional.of(account));
-        var result = accountService.getACMId(id);
+    void getAllAccounts() {
+        List<Account> accountList = new ArrayList<>();
+        accountList.add(EntityCreator.getAccount());
+        when(accountRepository.findAll()).thenReturn(accountList);
+        List<Account> result = accountService.getAllAccounts();
         Assertions.assertNotNull(result);
-        //Assertions.assertEquals(accountDto, result);
     }
 
     @Test
-    void getACMId_ShouldAccountDTOThrowException() {
+    void getAccById() {
+        Account account = EntityCreator.getAccount();
+        when(accountRepository.getReferenceById(account.getId())).thenReturn(account);
+        Account result = accountService.getAccById(account.getId());
+        Assertions.assertEquals(account, result);
+    }
+
+    @Test
+    void findAccountByAccountNumber() {
+        Account account = EntityCreator.getAccount();
+        when(accountRepository.findAccountByAccountNumber(account.getAccountNumber())).thenReturn(account);
+        Account result = accountService.findAccountByAccountNumber((account.getAccountNumber()));
+        Assertions.assertEquals(account, result);
+    }
+
+    @Test
+    void getACMIdTest() {
+        Account account = EntityCreator.getAccount();
+        AccountDto accountDto = DtoCreator.getAccountDto();
+        when(accountRepository.findById(account.getId())).thenReturn(Optional.of(account));
+        when(accountMapper.toDto(account)).thenReturn(accountDto);
+
+        AccountDto actualAccountDto = accountService.getACMId(account.getId());
+        Assert.assertEquals(actualAccountDto, accountDto);
+    }
+
+    @Test
+    void getACMId_AccountDTOThrowException() {
         UUID id = EntityCreator.getAccount().getId();
         when(accountRepository.findById(id)).thenReturn(Optional.empty());
         Assertions.assertThrows(AccountByIdNotFoundException.class, () -> accountService.getACMId(id));
