b212a0e462181d9a07dc71c666684a447830452e
Dmitri.Djourov <Dmitri.Djourov@gmail.com>
2024-02-27 22:47:46 +0200

    created ClientControllerTest.java

---
+++ src/test/java/com/djourov/bankapp/controller/rest/ClientControllerTest.java
@@ -0,0 +1,67 @@
+package com.djourov.bankapp.controller.rest;
+
+import com.djourov.bankapp.dto.ClientDto;
+import com.djourov.bankapp.entity.Client;
+import com.djourov.bankapp.service.impl.ClientServiceImpl;
+import com.djourov.bankapp.util.DtoCreator;
+import com.djourov.bankapp.util.EntityCreator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+@WebMvcTest(ClientController.class)
+@DisplayName("ClientController test class")
+class ClientControllerTest {
+
+    @Autowired
+    ClientController clientController;
+
+    @MockBean
+    private ClientServiceImpl clientService;
+
+    @Autowired
+    private MockMvc mockMvc;
+    private final Client client = EntityCreator.getClient();
+
+    @Test
+    void getAllClientsTest() throws Exception {
+        List<Client> clients = new ArrayList<>();
+        clients.add(client);
+        when(clientService.getAllClients()).thenReturn(clients);
+        mockMvc.perform(get("/app/client/clients"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getClientByIdTest() throws Exception {
+        ClientDto clientDto = DtoCreator.getClientDto();
+        when(clientService.getClientById(client.getId())).thenReturn(clientDto);
+        mockMvc.perform(get("/app/client/{id}", client.getId()))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    void getClientActiveTest() throws Exception {
+        int status = 0;
+        mockMvc.perform(get("/app/client/active/{status}", status))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray());
+    }
+}
