b530dca6a02fca79612409b2f7dd31a01a1bd98c
Dmitri.Djourov <Dmitri.Djourov@gmail.com>
2024-03-10 11:11:58 +0200

    added authenticationProvider

--- src/main/java/com/djourov/bankapp/config/SecurityConfig.java
+++ src/main/java/com/djourov/bankapp/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.djourov.bankapp.config;
 
+import com.djourov.bankapp.service.impl.ManagerDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,12 +14,22 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final ManagerDetailsServiceImpl managerDetailsService;
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(managerDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
