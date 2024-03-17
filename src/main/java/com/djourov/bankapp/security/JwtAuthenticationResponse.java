fdaff81c7abed7e7a30a71ff7a4eab5d493397e7
Dmitri.Djourov <Dmitri.Djourov@gmail.com>
2024-03-17 17:01:11 +0200

    added JwtAuthenticationResponse

---
+++ src/main/java/com/djourov/bankapp/security/JwtAuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.djourov.bankapp.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+//@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtAuthenticationResponse {
+    // @Schema(description = "Токен доступа", example = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTYyMjUwNj...")
+    private String token;
+}
