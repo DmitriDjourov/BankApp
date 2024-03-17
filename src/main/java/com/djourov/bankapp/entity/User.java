6b7078ff7113a787d0030c1455d16b92a5f2a8a2
Dmitri.Djourov <Dmitri.Djourov@gmail.com>
2024-03-17 16:29:20 +0200

    added users and role

---
+++ src/main/java/com/djourov/bankapp/entity/User.java
@@ -0,0 +1,64 @@
+package com.djourov.bankapp.entity;
+
+import com.djourov.bankapp.entity.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @Column(name = "u_id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_seq")
+    @SequenceGenerator(name = "user_id_seq", sequenceName = "user_id_seq", allocationSize = 1)
+    private Long id;
+
+    @Column(name = "u_username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "u_password", nullable = false)
+    private String password;
+
+    @Column(name = "u_email", unique = true, nullable = false)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "u_role", nullable = false)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
