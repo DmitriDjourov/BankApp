828b170add1e939ff4e35b1ae26ba38d80a85e1b
Dmitri.Djourov <Dmitri.Djourov@gmail.com>
2024-03-20 00:32:35 +0200

    swagger_list up

--- src/main/java/com/djourov/bankapp/config/SecurityConfig.java
+++ src/main/java/com/djourov/bankapp/config/SecurityConfig.java
@@ -30,6 +30,8 @@ public class SecurityConfig {
 
 
     private static final String[] SWAGGER_LIST = {
+            "/bankapp/v3/**",
+            "/bankapp/v3/api-docs",
             "/v2/api-docs",
             "/v3/api-docs/**",
             "/swagger-resources/**",
